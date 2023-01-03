ad6547da7599dbf9f18f12f82120ceb494731e0a
SMOKINFRY <jcrooge@gmail.com>
2023-01-03 13:01:17 +0100

    first commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,7 @@
+<script>
+    import Login from "../lib/Login.svelte"
+</script>
+<Login />
+<h1>welcome to recipes for sara</h1>
+<h2>here you will find all you need</h2>
+<h3>fooshiz, fo shaz</h3>
