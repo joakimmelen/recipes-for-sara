b63bf15e3b1c21a6538f8278b50694e8567cff9e
SMOKINFRY <jcrooge@gmail.com>
2023-01-05 11:36:50 +0100

    added error handlers

--- src/routes/recipes/[recipeId]/+page.server.js
+++ src/routes/recipes/[recipeId]/+page.server.js
@@ -7,15 +7,15 @@ const pb = new PocketBase('http://127.0.0.1:8090');
 export const load = async () => {
  
   // const getRecipe = async () => {
-  //   try {
+    try {
       const recipe = await pb.collection("recipes").getFullList();
       return {
         recipe: recipe.map((recip) => recip.export())
       }
-    // } catch (err) {
-    //   console.error(err)
-    //   throw error
-    // }
+    } catch (err) {
+      console.error(err)
+      throw error
+    }
   
   
  }
